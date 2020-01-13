70a27cc22389ba33bece9c5c4b78ab796aefc12c
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-13 16:14:49 +0000

    Automatically backed up by Learn

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,9 +1,16 @@
 class Employee
   attr_reader :name, :department, :age, :employees
   
+  @@all = [] 
+  
   def initialize(name, salary)
       @name = name 
-      @salary = salary  
+      @salary = salary
+      @@all << self 
   end 
   
+  
+  def self.all 
+      @@all 
+  end 
 end
