de5c3ce0973d6c4da32d66f3e9b0107ec0eeac63
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-13 16:19:44 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -3,6 +3,10 @@ require_relative "lib/Employee"
 require 'pry'
 
 # name, salary, manager_name
+john = Employee.new("John", "60000", "Nick")
+zack = Employee.new("Zack", "40000", "Darryl")
+mike = Employee.new("Mike", "45000", "Nick")
+
 
 
 
