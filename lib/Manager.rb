d7a6736de8aaa126519e903c407979e209dc8fd4
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-13 16:13:16 +0000

    Automatically backed up by Learn

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,11 @@
 class Manager
-
+attr_reader :name, :department, :age, :employees
+  
+  def initialize(name, department, age, employees)
+      @name = name 
+      @department = department
+      @age = age 
+      @employees = employees 
+  end 
+  
 end
