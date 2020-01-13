70f5c5533d63001436308f2cdde35ef766843496
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-13 16:21:38 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -7,6 +7,9 @@ john = Employee.new("John", "60000", "Nick")
 zack = Employee.new("Zack", "40000", "Darryl")
 mike = Employee.new("Mike", "45000", "Nick")
 
+# name, department, age
+nick = Manager.new("Nick", "Outbound", "28")
+darryl = Manager.new("Darryl", "Inbound", "27")
 
 
 
